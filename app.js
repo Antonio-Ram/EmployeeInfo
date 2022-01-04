9070c07d3d593b1e15153cc8a5b804a4eb9e6cb9
Antonio <tonito@umich.edu>
2022-01-03 22:09:24 -0500

    Rest of the queries

--- app.js
+++ app.js
@@ -12,6 +12,35 @@ const db = mysql.createConnection(
     console.log('Connected to the tracker database.')
 );
 
+/*//GET all departments
 db.query(`SELECT * FROM departments`, (err, rows) => {
     console.log(rows);
-});
+});*/
+
+/*//GET a single department
+db.query(`SELECT * FROM departments WHERE id = 1`, (err,row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});*/
+
+/*//DELETE a department
+db.query(`DELETE FROM departments WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});*/
+
+/*//ADD a department
+const sql = `INSERT INTO departments (name)
+            VALUES (?)`;
+const params = ['InfoTech'];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});*/

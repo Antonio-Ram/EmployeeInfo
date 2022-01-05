b4839522235ed8f5906e1cc10d05427fb13bc0ba
Antonio <tonito@umich.edu>
2022-01-05 15:33:39 -0500

    Constraint will not work:

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,16 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER
-);
-
-CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    /* Foreign Key constraint*/
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
-INSERT INTO roles (title, salary)
-VALUES
-('Help Desk', 35000),
-('Networking', 45000),
-('HR Compliance', 60000),
-('Workplace Safetey', 55000);
-
 INSERT INTO departments (name)
 VALUES
 ('InfoTech'),
 ('HumRes'),
 ('Sales'),
 ('RnD');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Help Desk', 35000, 1),
+('Networking', 45000, 1),
+('HR Compliance', 60000, 2),
+('Workplace Safetey', 55000, 2);

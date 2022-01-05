c4299959394f7e7dc40de151de0a3c9695a20456
Antonio <tonito@umich.edu>
2022-01-05 14:56:49 -0500

    Roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,10 @@
+INSERT INTO roles (title, salary)
+VALUES
+('Help Desk', 35000),
+('Networking', 45000),
+('HR Compliance', 60000),
+('Workplace Safetey', 55000);
+
 INSERT INTO departments (name)
 VALUES
 ('InfoTech'),

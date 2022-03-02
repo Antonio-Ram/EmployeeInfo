1e71b43c49e6f38f65a261214c0ec786b6f30d4c
Antonio <tonito@umich.edu>
2022-03-02 12:48:33 -0500

    add info help

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const db = mysql.createConnection(
     console.log('Connected to the tracker database.')
 );
 
+
 inquirer.prompt(
     {
         type: "list",
@@ -30,8 +31,8 @@ inquirer.prompt(
         choices: ["View all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee"]
     }
 )
-.then(({ start }) => {
-    switch (start) {
+.then(({ Start }) => {
+    switch (Start) {
         case "View all departments":
                 const sql = `SELECT * FROM departments`;
             
@@ -40,16 +41,69 @@ inquirer.prompt(
                         res.status(500).json( {error: err.message });
                         return;
                     }
-                    console.table([
-                        {
-                           id: departments.id,
-                           name: departments.name
-                        }
-                    ])
+                    console.table(rows);
                 });
             break;         
     };
+    switch (Start) {
+        case "view all roles":
+            const sql = `SELECT roles.*, departments.name
+            AS department_name
+            FROM departments
+            LEFT JOIN roles
+            ON roles.department_id = departments.id`;
+
+            db.query(sql, (err, rows) => {
+                if (err) {
+                    res.status(500).json( {error: err.message });
+                    return;  
+                }
+                console.table(rows);
+            });
+    };
+    switch (Start) {
+        case "view all employees":
+            const sql = `SELECT * FROM employees`
+
+            db.query(sql, (err, rows) => {
+                if(err) {
+                    res.status(500).json({ error: err.message });
+                    return;
+                }
+                console.table(rows);
+            });
+    };
+    switch (Start) {
+        case "add a department":
+            inquirer.prompt(
+                {
+                    type: "name",
+                    name: "newDepartment",
+                    message: "What is the name of the department?"
+                }
+            ).then(({ newDepartment }) =>{
+                switch(newDepartment){
+                    case '':
+                    const sql = `INSERT INTO departments (name)
+                        VALUES (?)`;
+                        const params = [body.name];
+
+                    db.query(sql, params, (err, result) => {
+                            if(err) {
+                                res.status(400).json({ error: err.message });
+                                return;
+                            }
+                            res.json({
+                                message: 'success',
+                                data: body
+                            });
+                        
+                    });
+                }
+            });
+    };
 });
+
 
 // View all departments
 /*app.get('/api/departments', (req, res) => {
